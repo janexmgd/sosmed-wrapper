d2783cf6eb34b910a7ef4742d45e5b8097eb30a8
janexmgd <janexmgd@gmail.com>
2023-10-25 16:41:11 +0700

    using return new promise at fetch post

--- src/helper/wrapper/instaPostwrapper.js
+++ src/helper/wrapper/instaPostwrapper.js
@@ -1,28 +1,35 @@
 import instatouch from 'instatouch';
 import epochNow from '../epochNow.js';
-const instaPostWrapper = async (username) => {
-  try {
-    const fetchProfile = await instatouch.user(username, { count: 9999999 });
-    const data = {
-      username: username,
-      fetchAt: epochNow(),
-      postCount: fetchProfile.count,
-      posts: [],
-    };
-    for (let index = 0; index < fetchProfile.collector.length; index++) {
-      const collector = fetchProfile.collector[index];
-      const sc = collector.shortcode;
-      const url = `https://www.instagram.com/p/${sc}/`;
-      const eachPost = {
-        id: collector.id,
-        shortcode: sc,
-        url: url,
+
+const instaPostWrapper = (username) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const fetchProfile = await instatouch.user(username, {
+        count: 9999999,
+      });
+      const data = {
+        username: username,
+        fetchAt: epochNow(),
+        postCount: fetchProfile.count,
+        posts: [],
       };
-      data.posts.push(eachPost);
+      for (let index = 0; index < fetchProfile.collector.length; index++) {
+        const collector = fetchProfile.collector[index];
+        const sc = collector.shortcode;
+        const url = `https://www.instagram.com/p/${sc}/`;
+        const eachPost = {
+          id: collector.id,
+          shortcode: sc,
+          url: url,
+        };
+        data.posts.push(eachPost);
+        console.log(`post ==> ${data.posts.length}`);
+      }
+      resolve(data);
+    } catch (error) {
+      reject(error);
     }
-    return data;
-  } catch (error) {
-    return error;
-  }
+  });
 };
+
 export default instaPostWrapper;
