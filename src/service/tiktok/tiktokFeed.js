095ba50d021da178cf5916a134a11d11fa2c1d64
janexmgd <janexmgd@gmail.com>
2024-01-14 12:45:26 +0700

    adding error message

--- src/service/tiktok/tiktokFeed.js
+++ src/service/tiktok/tiktokFeed.js
@@ -47,6 +47,7 @@ const tiktokFeed = async (secUid, cursor) => {
     };
     const r = await axios.get(`https://www.tiktok.com/`);
     const combinedCookie = r.headers['set-cookie'].join('; ');
+    console.log(combinedCookie);
     const xTTParams = createXttParams(new URLSearchParams(param).toString());
     const res = await axios.get(
       'https://www.tiktok.com/api/post/item_list/?aid=1988&app_language=en&app_name=tiktok_web&battery_info=1&browser_language=en-US&browser_name=Mozilla&browser_online=true&browser_platform=Win32&browser_version=5.0%20%28Windows%20NT%2010.0%3B%20Win64%3B%20x64%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F107.0.0.0%20Safari%2F537.36%20Edg%2F107.0.1418.35&channel=tiktok_web&cookie_enabled=true&device_id=7002566096994190854&device_platform=web_pc&focus_state=false&from_page=user&history_len=3&is_fullscreen=false&is_page_visible=true&os=windows&priority_region=RO&referer=https%3A%2F%2Fexportcomments.com%2F&region=RO&root_referer=https%3A%2F%2Fexportcomments.com%2F&screen_height=1440&screen_width=2560&tz_name=Europe%2FBucharest&verifyFp=verify_lacphy8d_z2ux9idt_xdmu_4gKb_9nng_NNTTTvsFS8ao&webcast_language=en&msToken=7UfjxOYL5mVC8QFOKQRhmLR3pCjoxewuwxtfFIcPweqC05Q6C_qjW-5Ba6_fE5-fkZc0wkLSWaaesA4CZ0LAqRrXSL8b88jGvEjbZPwLIPnHeyQq6VifzyKf5oGCQNw_W4Xq12Q-8KCuyiKGLOw=&X-Bogus=DFSzswVL-XGANHVWS0OnS2XyYJUm',
@@ -60,8 +61,8 @@ const tiktokFeed = async (secUid, cursor) => {
         },
       }
     );
-    console.log(res.data.itemList);
-    if (res.data.itemList.length == 0) {
+    // console.log(res.data.itemList);
+    if (!res.data.itemList.length) {
       throw new Error('tiktok not returned data');
     }
     const postList = [];
