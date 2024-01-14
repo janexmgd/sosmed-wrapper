20c599d61bf5a84b610867e27bd6ea0b4d68979b
janexmgd <janexmgd@gmail.com>
2024-01-14 16:23:33 +0700

    editing header

--- src/service/tiktok/tiktokFeed.js
+++ src/service/tiktok/tiktokFeed.js
@@ -46,18 +46,30 @@ const tiktokFeed = async (secUid, cursor) => {
         count: '30',
         is_encryption: 1,
       };
-      const r = await axios.get(`https://www.tiktok.com/`);
-      const combinedCookie = r.headers['set-cookie'].join('; ');
+      // const r = await axios.get(`https://www.tiktok.com/`);
+      // const combinedCookie = r.headers['set-cookie'].join('; ');
       delete client.defaults.headers;
       const xTTParams = createXttParams(new URLSearchParams(param).toString());
       const res = await client({
         url: 'https://www.tiktok.com/api/post/item_list/?aid=1988&app_language=en&app_name=tiktok_web&battery_info=1&browser_language=en-US&browser_name=Mozilla&browser_online=true&browser_platform=Win32&browser_version=5.0%20%28Windows%20NT%2010.0%3B%20Win64%3B%20x64%29%20AppleWebKit%2F537.36%20%28KHTML%2C%20like%20Gecko%29%20Chrome%2F107.0.0.0%20Safari%2F537.36%20Edg%2F107.0.1418.35&channel=tiktok_web&cookie_enabled=true&device_id=7002566096994190854&device_platform=web_pc&focus_state=false&from_page=user&history_len=3&is_fullscreen=false&is_page_visible=true&os=windows&priority_region=RO&referer=https%3A%2F%2Fexportcomments.com%2F&region=RO&root_referer=https%3A%2F%2Fexportcomments.com%2F&screen_height=1440&screen_width=2560&tz_name=Europe%2FBucharest&verifyFp=verify_lacphy8d_z2ux9idt_xdmu_4gKb_9nng_NNTTTvsFS8ao&webcast_language=en&msToken=7UfjxOYL5mVC8QFOKQRhmLR3pCjoxewuwxtfFIcPweqC05Q6C_qjW-5Ba6_fE5-fkZc0wkLSWaaesA4CZ0LAqRrXSL8b88jGvEjbZPwLIPnHeyQq6VifzyKf5oGCQNw_W4Xq12Q-8KCuyiKGLOw=&X-Bogus=DFSzswVL-XGANHVWS0OnS2XyYJUm',
         headers: {
-          ...musicallyHeader,
+          'sdk-version': '2',
+          'user-agent':
+            'com.zhiliaoapp.musically/2022307030 (Linux; U; Android 10.0.0; en; Samsung; Build/OPR6.1674146527.017;tt-ok/3.10.0.2)',
+          'x-ss-req-ticket': '1674146527532',
+          'passport-sdk-version': '19',
+          'x-vc-bdturing-sdk-version': '2.2.1.i18n',
+          'x-tt-dm-status': 'login=0;ct=1;rt=7',
+          'x-tt-store-idc': 'useast5',
+          'x-tt-store-region': 'us',
+          'x-tt-store-region-src': 'uid',
+          cookie:
+            'install_id=7187123175168165678; store-country-code=us; store-country-code-src=did; store-idc=useast5; ttreq=1$1b75e317092d0c7549eb4cc58df50fae2a7a8664',
+          'accept-encoding': 'gzip',
           'User-Agent':
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35',
           'x-tt-params': xTTParams,
-          Cookie: combinedCookie,
+          // Cookie: combinedCookie,
         },
       });
       // console.log(res.data.itemList);
